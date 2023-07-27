39068636d743eae470d4e4022db23b7642c14bf3
artifactNU <simon.nilsson.oberg@gmail.com>
2023-07-27 20:42:35 +0200

    Create-AzureStorage.ps1

--- Create-AzureStorage.ps1
+++ Create-AzureStorage.ps1
@@ -1,22 +1,58 @@
 # Connect to your Azure account
 Connect-AzAccount
 
-# Variables for Storage Account and Blob Container
-$resourceGroupName = "MyResourceGroup"       # Replace with the desired resource group name
-$storageAccountName = "mystorageaccount"     # Replace with your desired storage account name
-$location = "East US"                        # Replace with your desired Azure region
-$containerName = "myblobcontainer"           # Replace with your desired blob container name
+# Function to create a new Azure Storage Account and Blob Container
+function New-AzStorageAccountWithContainer {
+    param (
+        [string]$ResourceGroupName,
+        [string]$StorageAccountName,
+        [string]$Location,
+        [string]$ContainerName
+    )
 
-# Create the new Storage Account
-$storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroupName `
-                                      -Name $storageAccountName `
+    try {
+        # Create the new Storage Account
+        $storageAccount = New-AzStorageAccount -ResourceGroupName $ResourceGroupName `
+                                              -Name $StorageAccountName `
+                                              -Location $Location `
+                                              -SkuName "Standard_LRS"  # Replace with the desired SKU (e.g., Standard_LRS, Standard_GRS, etc.)
+
+        # Create a new Blob Container in the Storage Account
+        $container = New-AzStorageContainer -Name $ContainerName `
+                                            -Context $storageAccount.Context `
+                                            -Permission Off
+
+        Write-Host "Storage Account '$StorageAccountName' created successfully in '$Location'."
+        Write-Host "Blob Container '$ContainerName' created successfully in Storage Account '$StorageAccountName'."
+    }
+    catch {
+        Write-Host "An error occurred while creating the resources: $_"
+    }
+}
+
+# Main script starts here
+try {
+    # Variables for Storage Account and Blob Container
+    $resourceGroupName = "MyResourceGroup"       # Replace with the desired resource group name
+    $storageAccountName = "mystorageaccount"     # Replace with your desired storage account name
+    $location = "East US"                        # Replace with your desired Azure region
+    $containerName = "myblobcontainer"           # Replace with your desired blob container name
+
+    # Check if the resource group exists, and create it if it doesn't
+    if (-not (Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue)) {
+        New-AzResourceGroup -Name $resourceGroupName -Location $location
+        Write-Host "Resource group '$resourceGroupName' created successfully in '$location'."
+    }
+    else {
+        Write-Host "Resource group '$resourceGroupName' already exists."
+    }
+
+    # Create the new Storage Account and Blob Container
+    New-AzStorageAccountWithContainer -ResourceGroupName $resourceGroupName `
+                                      -StorageAccountName $storageAccountName `
                                       -Location $location `
-                                      -SkuName "Standard_LRS"  # Replace with the desired SKU (e.g., Standard_LRS, Standard_GRS, etc.)
-
-# Create a new Blob Container in the Storage Account
-$container = New-AzStorageContainer -Name $containerName `
-                                    -Context $storageAccount.Context `
-                                    -Permission Off
-
-Write-Host "Storage Account '$storageAccountName' created successfully in '$location'."
-Write-Host "Blob Container '$containerName' created successfully in Storage Account '$storageAccountName'."
+                                      -ContainerName $containerName
+}
+catch {
+    Write-Host "An error occurred: $_"
+}
