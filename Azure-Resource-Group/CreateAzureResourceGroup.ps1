47a4d4b42663f2a8c5b424136bd2b8d11e8bc5b9
artifactNU <simon.nilsson.oberg@gmail.com>
2023-07-31 20:42:51 +0200

    CreateAzureResourceGroup.ps1

--- Azure-Resource-Group/CreateAzureResourceGroup.ps1
+++ Azure-Resource-Group/CreateAzureResourceGroup.ps1
@@ -1,13 +1,22 @@
 # Install and import the Az module (if not already installed)
-Install-Module -Name Az -Force
-Import-Module Az
+if (-not (Get-Module -Name Az -ErrorAction SilentlyContinue)) {
+    Install-Module -Name Az -Force
+}
+Import-Module Az -Force
 
-# Connect to your Azure account (you will be prompted to log in)
+# Connect to your Azure account (you will be prompted to log in if not already authenticated)
 Connect-AzAccount
 
 # Define the resource group name and location
 $resourceGroupName = "MyResourceGroup" # Replace with your desired resource group name
 $location = "East US" # Replace with your desired Azure region
 
-# Create the new Resource Group
-New-AzResourceGroup -Name $resourceGroupName -Location $location
+# Check if the resource group already exists, and create it if it doesn't
+$existingResourceGroup = Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if (-not $existingResourceGroup) {
+    # Create the new Resource Group
+    New-AzResourceGroup -Name $resourceGroupName -Location $location
+    Write-Host "Resource group '$resourceGroupName' created successfully in '$location'."
+} else {
+    Write-Host "Resource group '$resourceGroupName' already exists."
+}
