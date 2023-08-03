c0eb8d9b1b336a246403b9d909a7dd695ace99c5
artifactNU <simon.nilsson.oberg@gmail.com>
2023-08-03 16:09:42 +0200

    Backup-VM-Azure.ps1

--- VM-Deployment-Azure/Backup-VM-Azure.ps1
+++ VM-Deployment-Azure/Backup-VM-Azure.ps1
@@ -1,5 +1,8 @@
-# Connect to your Azure account
-Connect-AzAccount
+# Install Azure AD PowerShell module if not already installed
+# Install-Module -Name AzureAD
+
+# Connect to your Azure AD tenant
+# Connect-AzureAD
 
 # Define the resource group and VM names
 $resourceGroupName = "MyResourceGroup"
