03ef3859069ff98574d189161d402e4fe6dcea0d
artifactNU <simon.nilsson.oberg@gmail.com>
2023-08-05 21:14:13 +0200

    Update

--- Storage/azure-upload-script.ps1
+++ Storage/azure-upload-script.ps1
@@ -5,7 +5,7 @@ $storageAccountName = "mystorageaccount"      # Replace with your storage accoun
 $containerName = "myblobcontainer"            # Replace with your blob container name
 
 # Path to the local file you want to upload
-$fileToUpload = "C:\path\to\your\file.txt"     # Replace with the path to your local file
+$fileToUpload = "C:\path\to\your\file.txt"    # Replace with the path to your local file
 
 # Connect to your Azure account
 Connect-AzAccount
