0f980dbab9fb53f887c4aec6c34d28baec5275e0
artifactNU <simon.nilsson.oberg@gmail.com>
2023-08-05 21:00:29 +0200

    Update

--- VM-Deployment-Azure/DeployAzureVM.ps1
+++ VM-Deployment-Azure/DeployAzureVM.ps1
@@ -6,7 +6,7 @@
 
 # Variables for VM configuration
 $resourceGroupName = "MyResourceGroup"          # Replace with the desired resource group name
-$location = "East US"                           # Replace with your desired Azure region
+$location = "swedencentral"                     # Replace with your desired Azure region
 $vmName = "MyVM"                                # Replace with your desired VM name
 $vmSize = "Standard_B2s"                        # Replace with the desired VM size (e.g., Standard_B1s, Standard_D2s_v3)
 $adminUsername = "adminUser"                    # Replace with the desired admin username
@@ -16,7 +16,8 @@ $imageOffer = "WindowsServer"                   # Offer of the VM image (e.g., W
 $imageSKU = "2019-Datacenter"                   # SKU of the VM image (e.g., 2019-Datacenter, 18.04-LTS)
 $imageVersion = "latest"                        # Version of the VM image to use (e.g., latest, specific version)
 
-# Check if the resource group exists, and create it if it doesn't
+# --- Resource Group ---
+Write-Host "Checking if the resource group exists: $resourceGroupName"
 if (-not (Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue)) {
     New-AzResourceGroup -Name $resourceGroupName -Location $location
     Write-Host "Resource group created: $resourceGroupName"
@@ -25,7 +26,8 @@ else {
     Write-Host "Resource group already exists: $resourceGroupName"
 }
 
-# Create the VM configuration
+# --- VM Configuration ---
+Write-Host "Creating VM configuration for $vmName"
 $vmConfig = New-AzVMConfig -VMName $vmName -VMSize $vmSize
 
 # Use a PSCredential object to securely store the admin credentials
@@ -37,7 +39,8 @@ $vmConfig = Set-AzVMSourceImage -VM $vmConfig -PublisherName $imagePublisher -Of
                                 -Skus $imageSKU -Version $imageVersion
 $vmConfig = Add-AzVMNetworkInterface -VM $vmConfig -Id (Get-AzNetworkInterface -ResourceGroupName $resourceGroupName).Id
 
-# Check if the VM exists, and create it if it doesn't
+# --- Virtual Machine ---
+Write-Host "Checking if the virtual machine exists: $vmName"
 if (-not (Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -ErrorAction SilentlyContinue)) {
     New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $vmConfig
     Write-Host "Virtual Machine created: $vmName"
