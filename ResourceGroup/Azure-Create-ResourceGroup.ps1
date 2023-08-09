aac28b417f317192d89d20088a89de41c222b7a7
Simon Nilsson Öberg <simon.nilsson.oberg@gmail.com>
2023-08-09 22:07:10 +0200

    Update

--- ResourceGroup/Azure-Create-ResourceGroup.ps1
+++ ResourceGroup/Azure-Create-ResourceGroup.ps1
@@ -1,7 +1,13 @@
-
 # Define the resource group name and location
 $resourceGroupName = "MyResourceGroup" # Replace with your desired resource group name
-$location = "swedencentral" # Replace with your desired Azure region
+$location = "northeurope" # Replace with your desired Azure region ('westus', 'eastasia', 'northeurope' etc.)
+
+# Validate the location
+$validLocations = Get-AzLocation | Select-Object -ExpandProperty Location
+if (-not $validLocations.Contains($location)) {
+    Write-Error "Invalid location specified. Please enter a valid Azure region."
+    return
+}
 
 # Check if the resource group already exists, and create it if it doesn't
 $existingResourceGroup = Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
