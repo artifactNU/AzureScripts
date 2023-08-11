a7929bc8ff618245ab7002a7e6d8f94c4174ec43
artifactNU <simon.nilsson.oberg@gmail.com>
2023-08-11 17:18:07 +0200

    Update

---
+++ bash/resourcegroup/create-resourcegroup.sh
@@ -0,0 +1,20 @@
+# Define the variables
+resourceGroupName="MyResourceGroup"  # Replace with your desired resource group name
+location="northeurope"               # Replace with your desired Azure region ('westus', 'eastasia', 'northeurope' etc.)
+
+# Validate the location
+validLocations=$(az account list-locations --query "[].name" -o tsv)
+if [[ ! $validLocations =~ (^|[[:space:]])$location($|[[:space:]]) ]]; then
+    echo "Invalid location specified. Please enter a valid Azure region."
+    exit 1
+fi
+
+# Check if the resource group already exists, and create it if it doesn't
+existingResourceGroup=$(az group show --name $resourceGroupName --query id -o tsv 2>/dev/null)
+if [[ -z $existingResourceGroup ]]; then
+    # Create the new Resource Group
+    az group create --name $resourceGroupName --location $location
+    echo "Resource group '$resourceGroupName' created successfully in '$location'."
+else
+    echo "Resource group '$resourceGroupName' already exists."
+fi
