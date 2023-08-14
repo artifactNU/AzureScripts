1882cf1c614514aa7ee8b74249f6e39c1345e2d2
Simon Nilsson Öberg <simon.nilsson.oberg@gmail.com>
2023-08-14 20:57:32 +0200

    Update

--- PowerShell/VM/Deploy-VM.ps1
+++ PowerShell/VM/Deploy-VM.ps1
@@ -1,3 +1,10 @@
+#    Before running this script, make sure to:
+# 1. Install the Azure PowerShell module using the following PowerShell command:
+#    Install-Module -Name Az
+# 2. Connect to your Azure account using the following PowerShell command:
+#    Connect-AzAccount
+# 3. Replace placeholders with your desired values for resource group, VM details, and image information.
+
 # Define the variables
 $resourceGroupName = "MyResourceGroup"          # Replace with the desired resource group name
 $location = "northeurope"                       # Replace with your desired Azure region ('westus', 'eastasia', 'northeurope' etc.)
