89704dc981ab8010eabbd5c9f47b614a4ca6993f
Simon Nilsson Öberg <simon.nilsson.oberg@gmail.com>
2023-08-14 20:52:58 +0200

    Update

--- PowerShell/ResourceGroup/Create-ResourceGroup.ps1
+++ PowerShell/ResourceGroup/Create-ResourceGroup.ps1
@@ -1,3 +1,10 @@
+#    Before running this script, make sure to:
+# 1. Install the Azure PowerShell module using the following PowerShell command:
+#    Install-Module -Name Az
+# 2. Connect to your Azure account using the following PowerShell command:
+#    Connect-AzAccount
+# 3. Replace placeholders with your desired values for resource group name and location.
+
 # Define the variables
 $resourceGroupName = "MyResourceGroup"          # Replace with your desired resource group name
 $location = "northeurope"                       # Replace with your desired Azure region ('westus', 'eastasia', 'northeurope' etc.)
