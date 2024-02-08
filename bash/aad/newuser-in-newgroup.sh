a742d75f8931e75cc5b8545861aa58f61de4d0e2
Simon Nilsson Öberg <simon.nilsson.oberg@gmail.com>
2024-02-08 10:37:12 +0100

    Update

--- bash/aad/newuser-in-newgroup.sh
+++ bash/aad/newuser-in-newgroup.sh
@@ -28,25 +28,3 @@ az ad group create --display-name $newGroupName \
                    --output none
 
 echo "New group '$newGroupName' has been created in Azure AD."
-
-# Variables
-newUserDisplayName="John Doe"                     # Replace with the desired display name for the new user
-newUserUPN="john.doe@yourtenant.onmicrosoft.com"  # Replace with the desired user principal name (UPN) for the new user
-newUserPassword="YourSecurePassword"              # Replace with a strong and secure password for the new user
-newGroupName="YourNewGroupName"                   # Replace with the desired name for the new group
-
-# Create a new user in Azure AD
-az ad user create --display-name $newUserDisplayName \
-                  --user-principal-name $newUserUPN \
-                  --password $newUserPassword \
-                  --force-change-password-next-login \
-                  --output none
-
-echo "New user '$newUserDisplayName' has been created in Azure AD."
-
-# Create a new group in Azure AD
-az ad group create --display-name $newGroupName \
-                   --mail-nickname $newGroupName \
-                   --output none
-
-echo "New group '$newGroupName' has been created in Azure AD."
